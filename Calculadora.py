00f31a9c536c0a1f9c173aeef2943b9e54ab8a8d
Usan31 <usan31showtek@gmail.com>
2021-05-28 12:42:20 -0600

    Con funciones y un cambio al decorado

--- Calculadora.py
+++ Calculadora.py
@@ -1,26 +1,40 @@
+word = "La"
+word2 = "de estos numeros es:"
+word3 = """Elige una opcion
+
+    0: suma
+    1: resta
+    2: multiplicacion
+    3: division
+
+Eliges... : """
+
+
 def suma(x,y):
-    suma = x + y
-    suma = round(suma) 
-    print(f"la suma de estos numeros es: {suma}")
+    suma = round(x + y) 
+    print(f"{word} suma {word2} {suma}")
 
 def resta(x,y):
-    resta = x - y
-    resta = round(resta) 
-    print(f"la resta de estos numeros es: {resta}")
+    resta = round(x - y) 
+    print(f"{word} resta {word2} {resta}")
+
+def multi(x,y):
+    multi = round(x * y) 
+    print(f"{word} multiplicacion {word2} {multi}")
+
+def divi(x,y):
+    divi = round(x / y)
+    print(f"{word} division {word2} {divi}")
 
 x = float(input("inserta un numero:\n"))
 y = float(input("inserta otro:\n"))
-z = int(input("elige una opcion:\n"))
+z = int(input(word3))
 
 if z == 0:
     suma(x,y)
 elif z == 1:
     resta(x,y)
 elif z == 2:
-    multi = x * y
-    multi = round(multi)
-    print(f"la multiplicacion de estos numeros es: {multi}")
+    multi(x,y)
 elif z == 3: 
-    divi = x / y
-    divi = round(divi)
-    print(f"la division de estos numeros es: {divi}")
+    divi(x,y)
